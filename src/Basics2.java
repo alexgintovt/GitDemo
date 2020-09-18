25417e2622c2f74f765fd6c03da3228ae8f371b9
Alex Gintovt <alexandr.gintovt@gmail.com>
2020-09-18 23:27:59 +0100

    Another guy code

--- src/Basics2.java
+++ src/Basics2.java
@@ -2,6 +2,8 @@ public class Basics2 {
 
     public String validateHeader(){
         System.out.println("Header links validated");
+        System.out.println("Header links validated2");
+        System.out.println("Header links validated3");
 
         return "Pass";
     }
